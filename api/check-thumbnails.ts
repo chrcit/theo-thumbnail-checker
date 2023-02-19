d61276edeab395950265ed96aeb9d8f48874bbb7
Christian Cito <citochris@gmail.com>
2023-02-19 12:39:53 +0100

    switch to lambda (rss-parser unsupported)

--- api/check-thumbnails.ts
+++ api/check-thumbnails.ts
@@ -1,3 +1,4 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import Parser from "rss-parser";
 
 const getYTChannelViaRSS = async (youtubeId: string) => {
@@ -6,7 +7,6 @@ const getYTChannelViaRSS = async (youtubeId: string) => {
     "https://www.youtube.com/feeds/videos.xml?channel_id=" + youtubeId
   );
 
-  console.log(rssResponse);
   return rssResponse;
 };
 
@@ -15,17 +15,16 @@ const getYTChannelViaRSS = async (youtubeId: string) => {
 const YT_CHANNEL_ID = "UCbRP3c757lWg9M-U7TyEkXA"; // https://www.youtube.com/@t3dotgg
 const MAX_AGE_IN_SECONDS = 60 * 60 * 24 * 7; // 1 week
 
-export default async (req: Request) => {
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
   try {
-    // const channel = await getYTChannelViaRSS(YT_CHANNEL_ID);
+    const channel = await getYTChannelViaRSS(YT_CHANNEL_ID);
 
-    return new Response(JSON.stringify({}), { status: 200 });
+    return new Response(JSON.stringify({ channel }), { status: 200 });
   } catch (error) {
     console.error(error);
-    return new Response(error.message, { status: 500 });
+    response.status(500).json({ error: error.message });
   }
-};
-
-export const config = {
-  runtime: "edge",
-};
+}
